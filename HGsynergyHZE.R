039bf414cea10cc568be36f336efc3998294796d
Rainer K. Sachs, Professor Emeritus <sachs@math.berkeley.edu>
2017-08-11 06:51:19 -0700

    Update HGsynergyHZE.R
    minor correctioons

--- HGsynergyHZE.R
+++ HGsynergyHZE.R
@@ -44,8 +44,9 @@ HZES=function(Dose,L){                 #Calculate slopes for later use in calcul
   }# made a numerical check at dose 10 for L=193. Got agreement to six significant figures
 #Looks OK up to here
 dose=c(seq(0, .00001, by = 0.000001),seq(.00002,.0001,by=.00001),seq(.0002,.001,by=.0001),seq(.002,.01,by=.001),seq(.02,.5,by=.01))##look carefully near zero, but go out to 0.5 Gy
+#dose=dose[1:30] #can be used to zoom in on very low doses
 #We will adapt Dae's use of ode() and uniroot() to our model and our data but all the rest of from Dae is irrelevant to us.
-MIXDER_function = function(r, L,d =dose,aa1=.7957,aa2=.003378,kk1=5.805) {
+MIXDER_function = function(r, L,d =dose,aa1=HZEc[1],aa2=HZEc[2],kk1=HZEc[3]) {
    dE=function(yini,State,Pars){
      aa1=aa1;aa2=aa2;kk1=kk1
      with(as.list(c(State, Pars)), {
