87b10c481803f357d47051cc0258aa1489eac395
Edward Huang <edwardghg@gmail.com>
2017-08-22 16:55:07 -0700

    more merged code

--- HGsynergyMain_merge.R
+++ HGsynergyMain_merge.R
@@ -9,6 +9,9 @@
 # "16Srn"=Siranart et al."Mixed Beam Murine Harderian Gland Tumorigenesis: Predicted Dose-Effect Relationships if neither Synergism nor Antagonism Occurs." Radiat Res 186(6): 577-591 (2016).  
 # "17Cuc"=Cucinotta & Cacao. "Non-Targeted Effects Models Predict Significantly Higher Mars Mission Cancer Risk than Targeted Effects Models." Sci Rep 7(1): 1832. (2017). PMC5431989
 
+library(deSolve) # library for solving differential equations
+library(minpack.lm) #for non-linear regression package
+
 dfr <- data.frame( # data used in 16Chang  
   Dose = c(0.2,0.4,0.6,1.2,2.4,3.2,5.1,7,0.05,0.1,0.15,0.2,0.4,0.8,1.6,0.05,0.1,0.2,0.4,0,0.1,0.2,0.4,0.8,1.6,0.4,0.8,1.6,3.2,0.05,0.1,0.2,0.4,0.8,0.1,0.2,0.4,0.8,0.1,0.2,0.4,0.04,0.08,0.16,0.32,0.033,0.066,0.13,0.26,0.52,.2, .4, .6),
   HG = c(0.091,0.045,0.101,0.169,0.347,0.431,0.667,0.623,0.156,0.215,0.232,0.307,0.325,0.554,0.649,0.123,0.145,0.207,0.31,0.026,0.083,0.25,0.39,0.438,0.424,0.093,0.195,0.302,0.292,0.109,0.054,0.066,0.128,0.286,0.183,0.167,0.396,0.536,0.192,0.234,0.317,0.092,0.131,0.124,0.297,0.082,0.088,0.146,0.236,0.371,.154,.132,.333),#HG prevalence as defined in 16Chang
@@ -22,4 +25,34 @@ dfr <- data.frame( # data used in 16Chang
   MeVperu = c(rep(228, 8), rep(600, 7), rep(300, 4), rep(600, 6), rep(250, 4), rep(670, 5), rep(600, 4), rep(600, 3), rep(260, 4), rep(1000, 5), rep(593, 3)),#Kinetic energy in MeV, divided by atomic mass, e.g. divided by 4u=4x931.5 MeV/c^2 for 2He4
   Katz = c(rep("TBD", 53)),#for fully ionized nuclei, Katz's Z^2/beta^2, Calculated below. It is part of the Bethe Barkas Bloch equation for stopping power. Our calculations don't use Katz, but various similar calculations do.
   ion = c(rep("He4", 8), rep("Fe56", 17), rep("p", 4), rep("Ne20", 5), rep("Nb93", 4), rep("Fe56", 3), rep("Si28", 4), rep("Ti48", 5), rep("La139", 3)),
-  comments = c(".93AlpLooksOK", rep("", 7), ".93AlplooksOK", rep('', 11), ".93Alp.no.iso", "not in 17Cuc (or 16Chang?)", rep("", 3), "16Chang all OK?", rep('', 24), ".94Alp","From graphs",'e.g. in 17Cuc')) 
+  comments = c(".93AlpLooksOK", rep("", 7), ".93AlplooksOK", rep('', 11), ".93Alp.no.iso", "not in 17Cuc (or 16Chang?)", rep("", 3), "16Chang all OK?", rep('', 24), ".94Alp","From graphs",'e.g. in 17Cuc')
+) 
+
+### Data for HG induced by photons from Cs-137 or Co-60 beta decay; from 16Chang
+ddd <- data.frame(Dose = c(0, 0.4, 0.8, 1.6, 3.2, 7, 0, .4, .8, .12, 1.6),
+                  HG = c(.026, .048, .093, .137, .322, .462, .0497, .054, .067, .128, .202),
+                  NWeight = c(6081.2, 4989.5, 1896.8, 981.1, 522.2, 205.2, 7474.1, 2877.6, 1423.7, 689.9, 514.9),
+                  Nucleus = c(rep("Cobalt-60", 6), rep("Cesium-137", 5)),
+                  Comments = c(rep("TBD", 11))
+)
+
+LQ <- lm(HG ~ Dose + I(Dose ^ 2), data = ddd) # linear model fit on ddd dataset
+summary(LQ, correlation = T) 
+
+Y <- 0.001 * dfr[, "MeVperu"] # convert to GeV/u for convenience in a calculation
+dfr[, "Katz"] <- round((dfr[, "Z"]) ^2 * (2.57 * Y^2 + 4.781 * Y + 2.233) / (2.57 * Y^2 + 4.781 * Y), 2) # special relativistic calculation of Z^2/beta^2. The numerics include conversion from GeV to joules and from u to kg.
+dfr[, "beta"] <- round(dfr[, "Z"] * sqrt(1 / dfr[, "Katz"]), 3) # i.e. Z*sqrt(beta^2/Z^2) 
+dfr[, "Zeff"] <- round(dfr[, "Z"] * (1 - exp( -125 * dfr[, "Z"] ^ (-2.0 / 3))), 2) # Barkas formula for Zeff; for us Zeff is almost Z
+
+dfra=dfr[c(1:19,26:53),] ##removes the zero dose case and the no isograft data
+
+##HERE OUR NEW HZE/NTE MODEL; works well. Uses 3 adjustable parameters. There is also an HZE/TE model and NTE or TE models for Z<=3, but this script only has the HZE/NTE model: all models are for Z>3 and all data is for Z>=8  ## 
+dfrHZE=subset(dfra,Z>3) #look only at HZE not at much lower LET ions. #In next line phi controls how fast NTE build up from zero; not really needed during calibration since 150*phi*Dose/L>>1 at every observed Dose !=0. phi needed for later synergy calculations
+phi=1000# Even larger phi should give the same final results, but might cause extra problems with R.
+HZEm=nls(HG~.0275+(1-exp(-0.01*(aa1*L*Dose*exp(-aa2*L)+(1-exp(-150*phi*Dose/L))*kk1))),data=dfrHZE, weights=NWeight,start=list(aa1=.9,aa2=.003, kk1=6))#calibrating parameters; 
+summary(HZEm,correlation=T)#; vcov(HZEm)# parameter values & accuracy; variance-covariance matrix
+HZEc=coef(HZEm)#calibrated central values of the 3 parameters.  Next is the IDER, =0 at dose 0
+HHC=function(Dose,L) 0.01*(HZEc[1]*L*Dose*exp(-HZEc[2]*L)+(1-exp(-150*phi*Dose/L))*HZEc[3])#calibrated hazard functions
+
+
+
