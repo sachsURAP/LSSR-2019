65f0dc72d7830da6412ad692577bb970169f4b83
Edward Huang <edwardghg@gmail.com>
2017-09-25 13:52:20 -0700

    cleaned up dataframe comments, removed redundant code

--- HGsynergyMain_merge2.R
+++ HGsynergyMain_merge2.R
@@ -1,5 +1,7 @@
-#   HGsynergyMain_merge2.R concerns radiogenic mouse HG tumorigenesis.
-#   Copyright (C) 2017 Mark Ebert, Edward Huang, Dae Woong Ham, Yimin Lin, and Ray Sachs
+#   Filename: HGsynergyMain_merge2.R 
+#   Purpose: Concerns radiogenic mouse HG tumorigenesis.
+
+#   Copyright: (C) 2017 Mark Ebert, Edward Huang, Dae Woong Ham, Yimin Lin, and Ray Sachs
  
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License version 3 as published 
@@ -16,30 +18,30 @@
 #   Attribution Information: This R script was developed at UC Berkeley.
 #   < authors and contributions to be added later > 
 
-#   Relevant references and their abbreviations in this script are the following:
+#   Relevant references and abbreviations:
 #   ".93Alp" = Alpen et al. "Tumorigenic potential of high-Z, high-LET charged-particle radiations." Rad Res 136:382-391 (1993)
 #   ".94Alp" = Alpen et al. "Fluence-based relative biological effectiveness for charged particle carcinogenesis in mouse Harderian gland." Adv Space Res 14(10): 573-581. (1994).  
 #   "16Chang" = Chang et al. "Harderian Gland Tumorigenesis: Low-Dose and LET Response." Radiat Res 185(5): 449-460. (2016).  
 #   "16Srn" = Siranart et al."Mixed Beam Murine Harderian Gland Tumorigenesis: Predicted Dose-Effect Relationships if neither Synergism nor Antagonism Occurs." Radiat Res 186(6): 577-591 (2016).  
 #   "17Cuc" = Cucinotta & Cacao. "Non-Targeted Effects Models Predict Significantly Higher Mars Mission Cancer Risk than Targeted Effects Models." Sci Rep 7(1): 1832. (2017). PMC5431989
 
-library(deSolve) #  Package for solving differential equations
-library(minpack.lm) #  Package for non-linear regression package
-rm(list=ls())
+library(deSolve) #  solving differential equations
+library(minpack.lm) #  non-linear regression 
+library(ggplot2) #  plotting
+library(mvtnorm) #  Monte Carlo simulation
 
-####### Data #######
+####### DATA #######
 dfr <- data.frame( # data used in 16Chang; includes data analyzed in .93Alp and .94Alp  
   dose.1 = c(0.2,0.4,0.6,1.2,2.4,3.2,5.1,7,0.05,0.1,0.15,0.2,0.4,0.8,1.6,0.05,0.1,0.2,0.4,0,0.1,0.2,0.4,0.8,1.6,0.4,0.8,1.6,3.2,0.05,0.1,0.2,0.4,0.8,0.1,0.2,0.4,0.8,0.1,0.2,0.4,0.04,0.08,0.16,0.32,0.033,0.066,0.13,0.26,0.52,.2, .4, .6),
-  HG = c(0.091,0.045,0.101,0.169,0.347,0.431,0.667,0.623,0.156,0.215,0.232,0.307,0.325,0.554,0.649,0.123,0.145,0.207,0.31,0.026,0.083,0.25,0.39,0.438,0.424,0.093,0.195,0.302,0.292,0.109,0.054,0.066,0.128,0.286,0.183,0.167,0.396,0.536,0.192,0.234,0.317,0.092,0.131,0.124,0.297,0.082,0.088,0.146,0.236,0.371,.154,.132,.333),#HG prevalence as defined in 16Chang
-  NWeight = c(520,2048,1145,584,313,232,293,221,1162,877,455,409,374,223,320,742,661,347,131,6081,1091,251,244,191,131,645,255,199,111,649,378,973,833,201,468,381,197,109,496,257,185,1902,1063,884,350,1767,1408,874,299,261,322,206,67),
-  #nominal weight for weighted least squaresregression; see .93Alp. The Lanthanum entries were obtained by measuring the main graph in 17Cuc 
-  index=c(rep(1,8),rep(0,17), rep(1,4),  rep(0,24)),#index=0 for Z>3 ions, 1 otherwise. Not needed in some models
-  L = c(rep(1.6,8), rep(193, 7), rep(250, 4), rep(195, 6), rep(0.4, 4), rep(25, 5), rep(464, 4), rep(193, 3),rep(70, 4), rep(100, 5), rep(953, 3)), ##L=LET=LET_infinity=stopping power (keV/micron)
-  Z = c(rep(2, 8), rep(26, 17), rep(1, 4), rep(10, 5), rep(43, 4), rep(26, 3), rep(14, 4), rep(22, 5), rep(57, 3)),#atomic number, charge in units of proton charge on fully ionized atomic nucleus, e.g. 2 for 2He4
-  Zeff = c(rep("TBD", 53)),# effective ion charge according to the formula of W.H Barkas. Zeff <= Z. Calculated below. For this data, only very slightly less than Z.
-  beta = c(rep("TBD", 53)),# ion speed, relative to speed of light, calculated below
-  MeVperu = c(rep(228, 8), rep(600, 7), rep(300, 4), rep(600, 6), rep(250, 4), rep(670, 5), rep(600, 4), rep(600, 3), rep(260, 4), rep(1000, 5), rep(593, 3)),#Kinetic energy in MeV, divided by atomic mass, e.g. divided by 4u=4x931.5 MeV/c^2 for 2He4
-  Katz = c(rep("TBD", 53)),#for fully ionized nuclei, Katz's Z^2/beta^2, Calculated below. It is part of the Bethe Barkas Bloch equation for stopping power. Our calculations don't use Katz, but various similar calculations do.
+  HG = c(0.091,0.045,0.101,0.169,0.347,0.431,0.667,0.623,0.156,0.215,0.232,0.307,0.325,0.554,0.649,0.123,0.145,0.207,0.31,0.026,0.083,0.25,0.39,0.438,0.424,0.093,0.195,0.302,0.292,0.109,0.054,0.066,0.128,0.286,0.183,0.167,0.396,0.536,0.192,0.234,0.317,0.092,0.131,0.124,0.297,0.082,0.088,0.146,0.236,0.371,.154,.132,.333), #  HG prevalence as defined in 16Chang
+  NWeight = c(520,2048,1145,584,313,232,293,221,1162,877,455,409,374,223,320,742,661,347,131,6081,1091,251,244,191,131,645,255,199,111,649,378,973,833,201,468,381,197,109,496,257,185,1902,1063,884,350,1767,1408,874,299,261,322,206,67), #  nominal weight for weighted least squaresregression; see .93Alp. The Lanthanum entries were obtained by measuring the main graph in 17Cuc 
+  index=c(rep(1,8),rep(0,17), rep(1,4),  rep(0,24)), #  index=0 for Z>3 ions, 1 otherwise. Not needed in some models
+  L = c(rep(1.6,8), rep(193, 7), rep(250, 4), rep(195, 6), rep(0.4, 4), rep(25, 5), rep(464, 4), rep(193, 3),rep(70, 4), rep(100, 5), rep(953, 3)), #  L = LET = LET_infinity = stopping power (keV/micron)
+  Z = c(rep(2, 8), rep(26, 17), rep(1, 4), rep(10, 5), rep(43, 4), rep(26, 3), rep(14, 4), rep(22, 5), rep(57, 3)), #  atomic number, charge in units of proton charge on fully ionized atomic nucleus, e.g. 2 for 2He4
+  Zeff = c(rep("TBD", 53)), #  effective ion charge according to the formula of W.H Barkas. Zeff <= Z. Calculated below. For this data, only very slightly less than Z.
+  beta = c(rep("TBD", 53)), #  ion speed, relative to speed of light, calculated below
+  MeVperu = c(rep(228, 8), rep(600, 7), rep(300, 4), rep(600, 6), rep(250, 4), rep(670, 5), rep(600, 4), rep(600, 3), rep(260, 4), rep(1000, 5), rep(593, 3)), #  Kinetic energy in MeV, divided by atomic mass, e.g. divided by 4u=4x931.5 MeV/c^2 for 2He4
+  Katz = c(rep("TBD", 53)), #  for fully ionized nuclei, Katz's Z^2/beta^2, Calculated below. It is part of the Bethe Barkas Bloch equation for stopping power. Our calculations don't use Katz, but various similar calculations do.
   ion = c(rep("He4", 8), rep("Fe56", 17), rep("p", 4), rep("Ne20", 5), rep("Nb93", 4), rep("Fe56", 3), rep("Si28", 4), rep("Ti48", 5), rep("La139", 3)),
   comments = c(".93AlpLooksOK", rep("", 7), ".93AlplooksOK", rep('', 11), ".93Alp.no.iso", "not in 17Cuc (or 16Chang?)", rep("", 3), "16Chang all OK?", rep('', 24), ".94Alp","From graphs",'e.g. in 17Cuc')
 ) 
@@ -51,12 +53,12 @@ ddd <- data.frame(dose.1 = c(0, 0.4, 0.8, 1.6, 3.2, 7, 0, .4, .8, .12, 1.6),
                   Nucleus = c(rep("Cobalt-60", 6), rep("Cesium-137", 5)),
                   Comments = c(rep("TBD", 11))
 )
-Y <- 0.001 * dfr[, "MeVperu"] # convert to GeV/u for convenience in a calculation
-dfr[, "Katz"] <- round((dfr[, "Z"]) ^2 * (2.57 * Y^2 + 4.781 * Y + 2.233) / (2.57 * Y^2 + 4.781 * Y), 2) # special relativistic calculation of Z^2/beta^2. The numerics include conversion from GeV to joules and from u to kg.
-dfr[, "beta"] <- round(dfr[, "Z"] * sqrt(1 / dfr[, "Katz"]), 3) # i.e. Z*sqrt(beta^2/Z^2) 
-dfr[, "Zeff"] <- round(dfr[, "Z"] * (1 - exp( -125 * dfr[, "Z"] ^ (-2.0 / 3))), 2) # Barkas formula for Zeff; for us Zeff is almost Z
+Y <- 0.001 * dfr[, "MeVperu"] #  convert to GeV/u for convenience in a calculation
+dfr[, "Katz"] <- round((dfr[, "Z"]) ^2 * (2.57 * Y^2 + 4.781 * Y + 2.233) / (2.57 * Y^2 + 4.781 * Y), 2) #  special relativistic calculation of Z^2/beta^2. The numerics include conversion from GeV to joules and from u to kg.
+dfr[, "beta"] <- round(dfr[, "Z"] * sqrt(1 / dfr[, "Katz"]), 3) #  i.e. Z*sqrt(beta^2/Z^2) 
+dfr[, "Zeff"] <- round(dfr[, "Z"] * (1 - exp( -125 * dfr[, "Z"] ^ (-2.0 / 3))), 2) #  Barkas formula for Zeff; for us Zeff is almost Z
 
-dfra <- dfr[c(1:19,26:53),] ##removes the zero dose case and the no isograft data
+dfra <- dfr[c(1:19,26:53),] #  removes the zero dose case and the no isograft data
 
 #####  photon and HZE/NTE Models #####
 LQ <- lm(HG ~ dose.1 + I(dose.1 ^ 2), data = ddd) # linear model fit on ddd dataset
@@ -106,17 +108,6 @@ IntegrateNTE_HZE_IMIXDER <- function(r, L, d = dose, aa1 = NTE.HZE.c[1], aa2 = N
 
 ####### Plotting I(d) example
 
-################################ TO DELETE #####################################
-# r <- c(1/3, 1/3, 1/3); L <- c(25, 70, 250)
-# INCRL <- IntegrateNTE_HZE_IMIXDER(r, L) # incremental effect additivity 
-# plot(INCRL, type='l', col='red', bty='l', ann='F') # ,ylim=c(0,.4) ; I(d) plot
-# lines(dose, CalculateHZEC(dose, 250), col='green') # component 3
-# lines(dose, CalculateHZEC(dose, 70), col='green') # component 2
-# lines(dose, CalculateHZEC(dose, 25), col='green') # component 1
-# SEA <- function(dose.1) CalculateHZEC(dose.1/3, 25) + CalculateHZEC(dose.1/3, 70) + CalculateHZEC(dose.1/3, 250)
-# lines(dose, SEA(dose), lty=2)
-################################ TO DELETE #####################################
-
 # another example
 # r <- .25*1:4; L <- c(25,70,190,250);plot(INCRL, type='l',ylim=c(0,.5),col='red',bty='l',ann='F')
 # lines(dose,CalculateHZEC(dose,190), col='green')#component 3
@@ -154,26 +145,6 @@ dE_1 <- function(d, aa1, aa2, kk1, phi, L) {
 dE_2 <- function(dose,L) { 
  LOW.c*exp(-LOW.c*dose)  
 }
-
-################################ TO DELETE #####################################
-# IntegrateNTE_HZE_LOW_IMIXDER <- function(r, L = 193, aa1 = NTE.HZE.c[1], aa2 = NTE.HZE.c[2], kk1 = NTE.HZE.c[3], phi = 3e3, beta = LOW.c, d) { 
-#   dE <- function(yini, State, Pars) {
-#     aa1 <- aa1; aa2 <- aa2; kk1 <- kk1;  beta <- beta; phi <- phi; L <- L
-#     with(as.list(c(State, Pars)), {
-#       u <- c(0, 0)
-#       u[1] <- uniroot(function(d) 1-exp(-0.01*(aa1*L*d*exp(-aa2*L)+(1-exp(-150*phi*d/L))*kk1)) - I, lower = 0, upper = 200, extendInt = "yes", tol = 10^-10)$root
-#       u[2] <- uniroot(function(d) 1-exp(-beta * d) - I, lower = 0, upper =200, extendInt = "yes", tol = 10^-10)$root#RKS changed upper to 200 instead of 5; cured lots of bugs; changed in u[1] also without helping or hurting. Maybe changing to upX or downX would be better still
-# 
-#       dI <- c(0, 0)
-#       dI[1] <- r[1] * dE_1(d = u[1], aa1 = aa1, aa2 = aa2, kk1 = kk1, phi = phi, L = L)
-#       dI[2] <- r[2] * dE_2(d = u[2], L = 0)
-#       dI <- sum(dI)
-#       return(list(c(dI)))
-#     })
-#   }
-#   return(ode(c(I = 0), times = d, dE, parms = NULL, method = "radau"))
-# }
-################################ TO DELETE #####################################
 
 # r1 <- .2; r <- c(r1, 1 - r1) #Proportions. Next plot IDERs and MIXDER
 # d <- .01 * 0:300.
