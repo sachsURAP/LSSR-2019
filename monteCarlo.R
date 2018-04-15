a27174d4528939e3d15ea57ae7ecc508dc9d461b
Edward Huang <edwardghg@gmail.com>
2018-04-15 16:26:28 -0700

    Changes: Added code to calculate uncorrelated MC CI, removed irrelevant CI call, commented out now obsolete "naive" CI code, made style changes with function headers and variable assignment, cleaned up progress print statements, fixed indexing call in generate_ci to work with small sample numbers, removed some obsolete variables in function environment.
    Remark: Code should be fully functional and should now run without error on small test samples. "naive" ci code should be fully removed on next commit.

--- monteCarlo.R
+++ monteCarlo.R
@@ -10,20 +10,15 @@ library(mvtnorm)
 #==============================================#
 
 # Parameter initialization
-# r <- c(0.05, 0.05, 0.05, 0.05, 0.8); L <- c(25, 70, 100, 195)
-sample_num = 200
-mod = 1              # 1 if HIN, 0 if HIT
-
-# d <- c(seq(0, .01, by = 0.001),
-       # seq(.02, .1, by=.01),
-       # seq(.2, 1, by=.1),
-       # seq(2, 100, by=1))
+sample_num <- 200
+mod <- 1              # 1 if HIN, 0 if HIT
 
 # Set the pseudorandom seed
 set.seed(100)
 
 # helper function to generate samples
-generate_samples <- function(N = sample_num, model = mod, HINmodel = HZE_nte_model, HITmodel = HZE_te_model, LOWmodel = low_LET_model, r, L, d) {
+generate_samples <- function(N = sample_num, r, L, d, model = mod, HINmodel = HZE_nte_model, 
+                             HITmodel = HZE_te_model, LOWmodel = low_LET_model, calib = TRUE) {
   # Function to generate Monte Carlo samples for calculating CI
   # @params:   N              - numbers of sample
   #            model          - select HIN or HIT model
@@ -35,13 +30,23 @@ generate_samples <- function(N = sample_num, model = mod, HINmodel = HZE_nte_mod
   monteCarloSamplesLow = rmvnorm(n = N, mean = coef(LOWmodel), sigma = vcov(LOWmodel))
   curve_list = list(0)
   if (model) {
-    monteCarloSamplesHin = rmvnorm(n = N, mean = coef(HINmodel), sigma = vcov(HINmodel))
+    if (calib) {
+      monteCarloSamplesHin <- rmvnorm(n = N, mean = coef(HINmodel), sigma = vcov(HINmodel))
+    } else {
+      monteCarloSamplesHin <- mapply(rnorm, rep(N, 3), coef(HINmodel), 
+                                     summary(HINmodel)$coefficients[, "Std. Error"])
+    }
   } else {
-    monteCarloSamplesHit = rmvnorm(n = N, mean = coef(HITmodel), sigma = vcov(HITmodel))
+    if (calib) {
+      monteCarloSamplesHit <- rmvnorm(n = N, mean = coef(HITmodel), sigma = vcov(HITmodel))
+    } else {
+      monteCarloSamplesHit <- mapply(rnorm, rep(N, 2), coef(HITmodel), 
+                                     summary(HITmodel)$coefficients[, "Std. Error"])
+      }
   }
   for (i in 1:N) {
     if (model) { 
-      curve_list[[i]] <- calculate_complex_id(r, L,d, coef = list(NTE = monteCarloSamplesHin[i, ],
+      curve_list[[i]] <- calculate_complex_id(r, L, d, coef = list(NTE = monteCarloSamplesHin[i, ],
                                                                  lowLET = monteCarloSamplesLow[i]),
                                             model = "NTE", lowLET = TRUE)
     } else {
@@ -50,15 +55,13 @@ generate_samples <- function(N = sample_num, model = mod, HINmodel = HZE_nte_mod
                                             model = "TE", lowLET = TRUE)
     }
     cat(paste("Currently at Monte Carlo step:", toString(i), "of", toString(N)), sprintf('\r'))
-    cat(sprintf('\r'))
   }
   return (curve_list)
 }
 
-# Generate N randomly generated samples of parameters of HZE model.
-# curve_list <- generate_samples(N = sample_num, r = r, L = L, d = d)
-
-generate_ci <- function(N = sample_num, intervalLength = 0.95, d, doseIndex, r, L, HINmodel = HZE_nte_model, HITmodel = HZE_te_model, LOWmodel = low_LET_model, method = 0, sampleCurves, model = mod) {
+generate_ci <- function(N = sample_num, intervalLength = 0.95, d, doseIndex, r, 
+                        L, HINmodel = HZE_nte_model, HITmodel = HZE_te_model, 
+                        LOWmodel = low_LET_model, method = 1, sampleCurves, model = mod) {
   # Function to generate CI for the input dose.
   # @params:   N              - numbers of sample
   #            intervalLength - size of confidence interval
@@ -79,93 +82,74 @@ generate_ci <- function(N = sample_num, intervalLength = 0.95, d, doseIndex, r, 
     }
     valueArr = sort(valueArr)
     # Returning resulting CI
-    return (c(valueArr[(1 - intervalLength) / 2 * N], valueArr[(intervalLength + (1 - intervalLength) / 2) * N]))
-  } else {
-    #========= Naive =========#
-    stdErrArrLow = summary(LOWmodel)$coefficients[, "Std. Error"]
-    meanArrLow = summary(LOWmodel)$coefficients[, "Estimate"]
-    if (model) {
-      stdErrArrHin = summary(HINmodel)$coefficients[, "Std. Error"]
-      meanArrHin = summary(HINmodel)$coefficients[, "Estimate"]
-      upper = calculate_complex_id(r, L, d = c(0, d),
-                                   coef = list(NTE = c(
-                                   meanArrHin["aa1"] + 2*stdErrArrHin["aa1"],
-                                   meanArrHin["aa2"] + 2*stdErrArrHin["aa2"],
-                                   meanArrHin["kk1"] + 2*stdErrArrHin["kk1"]),
-                                   lowLET = meanArrLow + 2*stdErrArrLow),
-                                   lowLET = TRUE)[, 2][2]
-      lower = calculate_complex_id(r, L, d = c(0, d),
-                                   coef = list(NTE = c(
-                                   meanArrHin["aa1"] - 2*stdErrArrHin["aa1"],
-                                   meanArrHin["aa2"] - 2*stdErrArrHin["aa2"],
-                                   meanArrHin["kk1"] - 2*stdErrArrHin["kk1"]),
-                                   lowLET = meanArrLow - 2*stdErrArrLow),
-                                   lowLET = TRUE)[, 2][2]
-    } else {
-      stdErrArrHit = summary(HITmodel)$coefficients[, "Std. Error"]
-      meanArrHit = summary(HITmodel)$coefficients[, "Estimate"]
-      upper = calculate_complex_id(r, L, d = c(0, d),
-                                   coef = list(TE = c(meanArrHit["aate1"] + 2*stdErrArrHit["aate1"],
-                                                      meanArrHit["aate2"] + 2*stdErrArrHit["aate2"]),
-                                               lowLET = meanArrLow + 2*stdErrArrLow),
-                                   model = "TE", lowLET = TRUE)[, 2][2]
-      lower = calculate_complex_id(r, L, d = c(0, d),
-                                   coef = list(TE = c(meanArrHit["aate1"] - 2*stdErrArrHit["aate1"],
-                                                      meanArrHit["aate2"] - 2*stdErrArrHit["aate2"]),
-                                               lowLET = meanArrLow - 2*stdErrArrLow),
-                                   model = "TE", lowLET = TRUE)[, 2][2]
-    }
-    return (c(lower, upper))
-  }
+    return (c(valueArr[ceiling((1 - intervalLength) / 2 * N)], valueArr[(intervalLength + (1 - intervalLength) / 2) * N]))
+  } 
+  # else {
+    # #========= Naive =========#
+    # stdErrArrLow = summary(LOWmodel)$coefficients[, "Std. Error"]
+    # meanArrLow = summary(LOWmodel)$coefficients[, "Estimate"]
+    # if (model) {
+    #   stdErrArrHin = summary(HINmodel)$coefficients[, "Std. Error"]
+    #   meanArrHin = summary(HINmodel)$coefficients[, "Estimate"]
+    #   upper = calculate_complex_id(r, L, d = c(0, d),
+    #                                coef = list(NTE = c(
+    #                                meanArrHin["aa1"] + 2*stdErrArrHin["aa1"],
+    #                                meanArrHin["aa2"] + 2*stdErrArrHin["aa2"],
+    #                                meanArrHin["kk1"] + 2*stdErrArrHin["kk1"]),
+    #                                lowLET = meanArrLow + 2*stdErrArrLow),
+    #                                lowLET = TRUE)[, 2][2]
+    #   lower = calculate_complex_id(r, L, d = c(0, d),
+    #                                coef = list(NTE = c(
+    #                                meanArrHin["aa1"] - 2*stdErrArrHin["aa1"],
+    #                                meanArrHin["aa2"] - 2*stdErrArrHin["aa2"],
+    #                                meanArrHin["kk1"] - 2*stdErrArrHin["kk1"]),
+    #                                lowLET = meanArrLow - 2*stdErrArrLow),
+    #                                lowLET = TRUE)[, 2][2]
+    # } else {
+    #   stdErrArrHit = summary(HITmodel)$coefficients[, "Std. Error"]
+    #   meanArrHit = summary(HITmodel)$coefficients[, "Estimate"]
+    #   upper = calculate_complex_id(r, L, d = c(0, d),
+    #                                coef = list(TE = c(meanArrHit["aate1"] + 2*stdErrArrHit["aate1"],
+    #                                                   meanArrHit["aate2"] + 2*stdErrArrHit["aate2"]),
+    #                                            lowLET = meanArrLow + 2*stdErrArrLow),
+    #                                model = "TE", lowLET = TRUE)[, 2][2]
+    #   lower = calculate_complex_id(r, L, d = c(0, d),
+    #                                coef = list(TE = c(meanArrHit["aate1"] - 2*stdErrArrHit["aate1"],
+    #                                                   meanArrHit["aate2"] - 2*stdErrArrHit["aate2"]),
+    #                                            lowLET = meanArrLow - 2*stdErrArrLow),
+    #                                model = "TE", lowLET = TRUE)[, 2][2]
+    # }
+    # return (c(lower, upper))
+  # }
 }
-
-# Parameter initialization
-# if (mod) {
-#   mixderCurve = calculate_complex_id(r, L, d = d, lowLET = TRUE)
-# } else {
-#   mixderCurve = calculate_complex_id(r, L, d = d, lowLET = TRUE, model = "TE")
-# }
-# fourIonMIXDER = data.frame(d = mixderCurve[, 1], CA = mixderCurve[, 2])
-# numDosePoints = length(fourIonMIXDER$d)
-# naive_ci = matrix(nrow = 2, ncol = numDosePoints)
-# monte_carlo_ci = matrix(nrow = 2, ncol = numDosePoints)
-
-# Calculate CI for each dose point
-# for (i in 1 : numDosePoints) {
-#   naive_ci[, i] = generate_ci(d = fourIonMIXDER$d[i], r = r,  L = L, sampleCurves = curve_list)
-#   monte_carlo_ci[, i] = generate_ci(doseIndex = i, r = r,  L = L, method = 1, sampleCurves = curve_list)
-#   cat(paste("Iterating on dose points. Currently at step:", toString(i), "of", toString(numDosePoints)), sprintf('\r'))
-#   
-# }
 
 #############
 # CI Helper #
 #############
-ci_helper = function(sample_num, intervalLength = 0.95, d, r, L, HINmodel = HZE_nte_model, HITmodel = HZE_te_model, LOWmodel = low_LET_model, model = mod, seed = 100) {
+ci_helper <- function(sample_num, intervalLength = 0.95, d, r, L,
+                      model = mod, seed = 100, calib = TRUE) {
   # Set the pseudorandom seed
   set.seed(seed)
-  
   # Generate N randomly generated samples of parameters of HZE model.
-  curve_list <- generate_samples(N = sample_num, r = r, L = L, d = d)
-  
+  curve_list <- generate_samples(N = sample_num, r = r, L = L, d = d, model = model, calib = calib)
   # Parameter initialization
   if (mod) {
     mixderCurve <- calculate_complex_id(r, L, d = d, lowLET = TRUE)
   } else {
     mixderCurve <- calculate_complex_id(r, L, d = d, lowLET = TRUE, model = "TE")
   }
-  fourIonMIXDER = data.frame(d = mixderCurve[, 1], CA = mixderCurve[, 2])
-  numDosePoints = length(fourIonMIXDER$d)
-  naive_ci = matrix(nrow = 2, ncol = numDosePoints)
+  numDosePoints = length(d)
+  # naive_ci = matrix(nrow = 2, ncol = numDosePoints)
   monte_carlo_ci = matrix(nrow = 2, ncol = numDosePoints)
   
   # Calculate CI for each dose point
   for (i in 1 : numDosePoints) {
-    naive_ci[, i] = generate_ci(d = fourIonMIXDER$d[i], r = r,  L = L, sampleCurves = curve_list)
-    monte_carlo_ci[, i] = generate_ci(doseIndex = i, r = r,  L = L, method = 1, sampleCurves = curve_list)
+    # naive_ci[, i] = generate_ci(d = fourIonMIXDER$d[i], r = r,  L = L, sampleCurves = curve_list)
+    monte_carlo_ci[, i] = generate_ci(N = sample_num, doseIndex = i, r = r,  L = L, model = model, method = 1, sampleCurves = curve_list)
     cat(paste("Iterating on dose points. Currently at step:", toString(i), "of", toString(numDosePoints)), sprintf('\r'))
   }
-  return (list(naive = naive_ci, monte_carlo = monte_carlo_ci))
+  # return (list(naive = naive_ci, monte_carlo = monte_carlo_ci))
+  return (list(monte_carlo = monte_carlo_ci))
 }
 
 #==============================================#
@@ -178,4 +162,3 @@ ci_helper = function(sample_num, intervalLength = 0.95, d, r, L, HINmodel = HZE_
 # lines(1:128, naive_ci[2, ], col = "red")
 # lines(1:128, monte_carlo_ci[1, ], col = "blue")
 # lines(1:128, monte_carlo_ci[2, ], col = "blue")
-
