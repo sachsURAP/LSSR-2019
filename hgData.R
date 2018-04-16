fe8f522ba25aa2a766ea6c69ccf8d2113a222b67
Edward Huang <edwardghg@gmail.com>
2018-04-16 12:18:42 -0700

    Updated header commenting, reformatted references for readability, added abbreviation definitions, shortened read.csv() call

--- hgData.R
+++ hgData.R
@@ -1,37 +1,58 @@
-#   Filename: Data.R 
-#   Purpose: Concerns radiogenic mouse HG tumorigenesis.
+# Copyright:    (C) 2017-2018 Sachs Undergraduate Research Apprentice Program
+#               This program and its accompanying materials are distributed 
+#               under the terms of the GNU General Public License v3.
+# Filename:     hgData.R 
+# Purpose:      Concerns radiogenic mouse Harderian gland tumorigenesis. Loads 
+#               ion and tumor prevalence data from CSV files. It is part of the 
+#               source code for the NASAmouseHG project.
+# Contact:      Rainer K. Sachs 
+# Website:      https://github.com/sachsURAP/NASAmouseHG
+# Mod history:  15 Apr 2018
+# Attribution:  This R script was developed at UC Berkeley. Written by Dae Woong 
+#               Ham Summer 2017. Additions, corrections, changes, quality 
+#               control, reorganization by Edward Huang, Yimin Lin, Mark Ebert,
+#               Yunzhi Zhang and Ray Sachs UCB 2017-2018 academic year.
 
-#   Copyright: (C) 2017 Mark Ebert, Edward Huang, Dae Woong Ham, Yimin Lin,
-#                       Yunzhi Zhang, and Ray Sachs.
+# Relevant references and abbreviations:
+#
+#   ".93Alp" = Alpen et al. "Tumorigenic potential of high-Z, high-LET charged-
+#                           particle radiations." Rad Res 136:382-391 (1993).
+#
+#   ".94Alp" = Alpen et al. "Fluence-based relative biological effectiveness for
+#                           charged particle carcinogenesis in mouse Harderian 
+#                           gland." Adv Space Res 14(10): 573-581. (1994).  
+#
+#   "16Chang" = Chang et al. "Harderian Gland Tumorigenesis: Low-Dose and LET 
+#                            Response." Radiat Res 185(5): 449-460. (2016). 
+#
+#   "16Srn" = Siranart et al. "Mixed Beam Murine Harderian Gland Tumorigenesis: 
+#                             Predicted Dose-Effect Relationships if neither 
+#                             Synergism nor Antagonism Occurs." 
+#                             Radiat Res 186(6): 577-591 (2016).  
+#
+#   "17Cuc" = Cucinotta & Cacao. "Non-Targeted Effects Models Predict 
+#                                Significantly Higher Mars Mission Cancer Risk 
+#                                than Targeted Effects Models." 
+#                                Sci Rep 7(1): 1832. (2017). PMC5431989
+#
+#   "HZE"     = High atomic number and energy
+#   "LET"     = Linear energy transfer
+#   "NTE"     = Non-targeted effects
+#   "TE"      = Targeted effects
+#   "IDER"    = Individual dose-effect relationship
+#   "MIXDER"  = Mixed dose-effect relationship
+#   "SEA"     = Simple Effect Additivity
+#   "IEA"     = Incremental Effect Additivity
+#   "cGy"     = Centigray
 
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License version 3 as published 
-#   by the Free Software Foundation.
+rm(list=ls()) # To be removed when script is finalized
 
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See 
-#   GNU GPLv3 for more details. <http://www.gnu.org/licenses/>.
+# Data used in 16Chang; includes data analyzed in .93Alp and .94Alp. Does not 
+# include gamma-ray data or ion background (i.e. control) data.
+# These two lines of script plus the .csv file (which will need work as 
+# additions and perhaps corrections come up) should be all we need.
+ion_data <- data.frame(read.csv("data/raw_data_ordered.csv")) 
 
-#   Attribution Information: This R script was developed at UC Berkeley.
-#   Written by Dae Woong Ham Summer 2017. Additions, corrections, changes, 
-#   quality control, reorganization by Edward Huang, Yimin Lin, Mark Ebert,
-#   Yunzhi Zhang and Ray Sachs UCB 2017-2018 academic year.
-
-#   Relevant references and abbreviations:
-#   ".93Alp" = Alpen et al. "Tumorigenic potential of high-Z, high-LET charged-particle radiations." Rad Res 136:382-391 (1993)
-#   ".94Alp" = Alpen et al. "Fluence-based relative biological effectiveness for charged particle carcinogenesis in mouse Harderian gland." Adv Space Res 14(10): 573-581. (1994).  
-#   "16Chang" = Chang et al. "Harderian Gland Tumorigenesis: Low-Dose and LET Response." Radiat Res 185(5): 449-460. (2016).  
-#   "16Srn" = Siranart et al."Mixed Beam Murine Harderian Gland Tumorigenesis: Predicted Dose-Effect Relationships if neither Synergism nor Antagonism Occurs." Radiat Res 186(6): 577-591 (2016).  
-#   "17Cuc" = Cucinotta & Cacao. "Non-Targeted Effects Models Predict Significantly Higher Mars Mission Cancer Risk than Targeted Effects Models." Sci Rep 7(1): 1832. (2017). PMC5431989
-
-rm(list=ls()) #   To be removed when script is finalized
-ion_data <- data.frame(read.csv("data/raw_data_ordered.csv", 
-                                sep = ",", 
-                                header = TRUE)) 
-#  Data used in 16Chang; includes data analyzed in .93Alp and .94Alp. Does not 
-#  include gamma-ray data or ion background (i.e. control) data.
-#  These two lines of script plus the .csv file (which will need work as 
-#  additions and perhaps corrections come up) should be all we need.
-#   However the following, which shows how to compute ion speed and the Katz 
-#  track structure parameter, may be useful later.
+# However the following, which shows how to compute ion speed and the Katz 
+# track structure parameter, may be useful later. 
+#EGH Apr 16: See older versions.
