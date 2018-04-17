d50d2ed482d3424990b0d174539c922cedb3eff3
Edward Huang <edwardghg@gmail.com>
2018-04-17 14:43:48 -0700

    Refactored .generate_ci, shortened parameter documentation, removed obsolete commented code, removed interval-generation print statements since the loop is too fast to read.

--- monteCarlo.R
+++ monteCarlo.R
@@ -19,38 +19,36 @@ library(mvtnorm) # Sampling
 #======================= MONTE CARLO SIMULATION FUNCTION ======================#
 
 #' @description Runs the Monte Carlo method on a MIXDER.
+#' 
 #' @param sample_num Numeric integer of the number of samples to be drawn.
 #' @param d Numeric vector of all total dose values to be evaluated. 
-#' @param r Numeric vector of the proportions of dose applied to each component
-#'          IDER.
+#' @param r Numeric vector of the dose proportions applied to component IDERs.
 #' @param l Numeric vector of all LET values, must be length n.
-#' @param model String value corresponding to the model to be used, either 
-#'              "NTE" or "TE". 
+#' @param model String value corresponding to the model to be used. 
 #' @param seed Numeric value for pseudorandom generators.
-#' @param corr Boolean for whether the Monte Carlo should assess correlation
-#'             between sampled parameters.
+#' @param corr Boolean for assessing inter-parameter correlation.
+#'             
 #' @details Corresponding elements of ratios, LET should be associated with the
 #'          same IDER.
+#'          
 #' @return Numeric vector representing lower and upper bounds for a Monte Carlo
 #'         confidence interval for the given MIXDER at the given dosages.
+#'         
 #' @examples
-#' 
-#' 
+
 simulate_monte_carlo <- function(sample_num = 200, d, r, L,
                                  model = "NTE", seed = 100, corr = TRUE) {
   # Set the pseudorandom seed
   set.seed(seed)
   # Generate N randomly generated samples of parameters of HZE model.
-  curve_list <- .generate_samples(N = sample_num, r = r, L = L, d = d, 
-                                  model = model, corr = corr)
+  curve_list <- .generate_samples(sample_num, d, L, r, model, corr)
   numDosePoints <- length(d)
   monte_carlo_ci <- matrix(nrow = 2, ncol = numDosePoints)
   
   # Calculate CI for each dose point
-  for (i in 1 : numDosePoints) {
-    monte_carlo_ci[, i] <- .generate_ci(N = sample_num, doseIndex = i, sampleCurves = curve_list)
-    cat(paste(" Iterating on dose points. Currently at step:", toString(i), "of", 
-              toString(numDosePoints)), sprintf('\r'))
+  for (i in 1 : numDosePoints) { #EGH: Possible vectorization opportunity
+    monte_carlo_ci[, i] <- .generate_ci(N = sample_num, dose_index = i, 
+                                        sample_curves = curve_list)
   }
   return(list(monte_carlo = monte_carlo_ci))
 }
@@ -61,48 +59,33 @@ simulate_monte_carlo <- function(sample_num = 200, d, r, L,
 
 #' @description Generates MIXDER samples using parameters drawn from a Gaussian 
 #'              distribution.
+#'              
 #' @param N Numeric integer of the number of samples to be drawn.
 #' @param d Numeric vector of all total dose values to be evaluated. 
-#' @param r Numeric vector of the proportions of dose applied to each component
-#'          IDER.
+#' @param r Numeric vector of the dose proportions applied to component IDERs.
 #' @param l Numeric vector of all LET values, must be length n.
-#' @param model String value corresponding to the model to be used, either 
-#'              "NTE" or "TE". 
+#' @param model String value corresponding to the model to be used.
+#' @param corr Boolean for assessing inter-parameter correlation. 
+#'             TRUE iff correlations are being taken into account. #RKS to EGH. Is this correct? #EGH Apr 16: Correct. I will update function documentation in the next few commmits.
 #' @param HINmodel The input HIN model
 #' @param HITmodel The input HIT model # RKS to EGH. Corrected misprint. #EGH Apr 16: Thanks.
 #' @param LOWmodel The input LOW model
-#' @param corr Boolean for whether the Monte Carlo should assess correlation
-#'             between sampled parameters. TRUE iff correlations are being taken into account. #RKS to EGH. Is this correct? #EGH Apr 16: Correct. I will update function documentation in the next few commmits.
+#' 
 #' @details Corresponding elements of ratios, LET should be associated with the
 #'          same IDER.
+#'          
 #' @return Numeric vector of sample MIXDERs evaluated at the given dosages. 
+#' 
 #' @examples
-#' 
-#' 
-.generate_samples <- function(N = 200, r, L, d, model, HINmodel = HZE_nte_model, 
-                             HITmodel = HZE_te_model, LOWmodel = low_LET_model, 
-                             corr = TRUE) {
+
+.generate_samples <- function(N = 200, d, L, r, model, corr, 
+                              HINmodel = HZE_nte_model, HITmodel = HZE_te_model, 
+                              LOWmodel = low_LET_model) {
   # monteCarloSamplesLow <- rmvnorm(n = N, mean = coef(LOWmodel), sigma = vcov(LOWmodel)) # RKS to RKS and EGH. Is vcov a 1x1 matrix here? #EGH Apr 16: Correct. 
-  low_LET_samples <- rmvnorm(n = N, mean = coef(LOWmodel), sigma = vcov(LOWmodel))
+  low_LET_samples <- rmvnorm(N, coef(LOWmodel), vcov(LOWmodel))
   # RKS to EGH. I worried that your Monte Carlo was fast whereas that of Dae and the CA pod is much slower.
   # So I tried to understand the next chunk that defines curve_list, but got lost. #EGH Apr 16: That's fair. Funnily enough, I was worried that this Monte Carlo seemed too slow, and I was looking into ways to speed it up.
   # How should we handle this kind of situation to not lose information but not clutter up the script? #EGH Apr 16: Are you familiar with the browser() function? It allows you to freeze a function mid-evaluation and examine its environment and make other diagnostic function calls. 
-  curve_list <- list(0)
-  # if (model) {
-  #   if (corr) {
-  #     monteCarloSamplesHin <- rmvnorm(n = N, mean = coef(HINmodel), sigma = vcov(HINmodel)) # RKS to RKS and EGH. Here 3x3? #EGH Apr 16: Correct.
-  #   } else {
-  #     monteCarloSamplesHin <- mapply(rnorm, rep(N, 3), coef(HINmodel), 
-  #                                    summary(HINmodel)$coefficients[, "Std. Error"])
-  #   }
-  # } else {
-  #   if (corr) {
-  #     monteCarloSamplesHit <- rmvnorm(n = N, mean = coef(HITmodel), sigma = vcov(HITmodel))
-  #   } else {
-  #     monteCarloSamplesHit <- mapply(rnorm, rep(N, 2), coef(HITmodel), 
-  #                                    summary(HITmodel)$coefficients[, "Std. Error"])
-  #   }
-  # }
   if (model == "NTE") {
     ion_model <- HINmodel
     num_coef <- 3
@@ -116,7 +99,9 @@ simulate_monte_carlo <- function(sample_num = 200, d, r, L,
     samples <- mapply(rnorm, rep(N, num_coef), coef(ion_model), 
                       summary(ion_model)$coefficients[, "Std. Error"])
   }
-  for (i in 1:N) {
+  curve_list <- list(0)
+  
+  for (i in 1:N) { #EGH: Possible vectorization opportunity. Next block constitutes the bulk of the Monte Carlo runtime.
     if (model == "NTE") {
       curve_list[[i]] <- calculate_complex_id(r = r, LET = L, d = d, 
                                               coef = list(NTE = samples[i, ],
@@ -128,44 +113,33 @@ simulate_monte_carlo <- function(sample_num = 200, d, r, L,
                                               lowLET = low_LET_samples[i]),
                                               model = "TE", lowLET = TRUE)
     }
-    # if (model) { 
-    #   curve_list[[i]] <- calculate_complex_id(r = r, LET = L, d = d, 
-    #                                           coef = list(NTE = monteCarloSamplesHin[i, ],
-    #                                           lowLET = monteCarloSamplesLow[i]),
-    #                                           model = "NTE", lowLET = TRUE)
-    # } else {
-    #   curve_list[[i]] <- calculate_complex_id(r = r, LET = L, d = d, 
-    #                                           coef = list(TE = monteCarloSamplesHit[i, ],
-    #                                           lowLET = monteCarloSamplesLow[i]),
-    #                                           model = "TE", lowLET = TRUE)
-    # }
-    cat(paste(" Currently at Monte Carlo step:", toString(i), "of", toString(N)), sprintf('\r'))
+    cat(paste("  Currently at Monte Carlo step:", toString(i), 
+              "of", toString(N)), sprintf('\r'))
   }
   return(curve_list)
 }
 
 #============= INTERVAL CONSTRUCTION ===========#
 
-#' @description Generates confidence interval bounds for given MIXDER samples at
-#'              a given dosage.
+#' @description Generates confidence interval bounds for given MIXDER samples 
+#'              at a given dosage.
+#'              
 #' @param N Numeric integer of the number of samples to be drawn.
-#' @param intervalLength Numeric double of the confidence interval width 
-#' @param doseIndex Numeric integer of the dose 
-#' @param sampleCurves List 
+#' @param interval_length Numeric double of the confidence interval width.
+#' @param dose_index Numeric integer of dosage.
+#' @param sample_curves Numeric list of sampled MIXDER values.
+#' 
 #' @details 
-#' @return Numeric length-two vector of an upper and lower bound for the confidence 
-#'         interval at the given dosage.
+#' 
+#' @return Numeric length-two vector of an upper and lower bound for the 
+#'         confidence interval at the given dosage.
+#'         
 #' @examples
-#' 
-#' 
-.generate_ci <- function(N, intervalLength = 0.95, doseIndex, sampleCurves) {
+
+.generate_ci <- function(N, interval_length = 0.95, dose_index, sample_curves) {
   # For each sample curve, evalute them at input dose, and sort.
-  valueArr <- vector(length = 0)
-  for (i in 1:N) {
-    valueArr <- c(valueArr, sampleCurves[[i]][, 2][doseIndex])
-  }
-  valueArr <- sort(valueArr)
+  sample_values <- sort(sapply(sample_curves, function(x) x[, 2][dose_index]))
   # Returning resulting CI
-  return(c(valueArr[ceiling((1 - intervalLength) / 2 * N)], 
-           valueArr[(intervalLength + (1 - intervalLength) / 2) * N]))
+  return(c(sample_values[ceiling((1 - interval_length) / 2 * N)], 
+           sample_values[(interval_length + (1 - interval_length) / 2) * N]))
 }
