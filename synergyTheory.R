bcd3d258143a534c4b81dd93062f707a0555f49f
Edward Huang <edwardghg@gmail.com>
2018-06-03 23:09:07 -0600

    Rewrote calculate_id to be LET sensitive, in other words, the LET boolean parameter is no longer necessary. new_calculate_id can handle MIXDERs of only lowLET ions. Cursory testing performed but additional testing is required.

--- synergyTheory.R
+++ synergyTheory.R
@@ -99,7 +99,7 @@ summary(low_LET_model, correlation = TRUE)
 low_LET_model_coef <- coef(low_LET_model) # Calibrated central values of the parameter
 
 # Calibrated Low LET model. Use L=0, but maybe later will use L > 0 but small
-calibrated_low_LET_der <- function(dose, LET, alph_low= low_LET_model_coef[1]) {  
+calibrated_low_LET_der <- function(dose, LET, alph_low = low_LET_model_coef[1]) {  
   return(1 - exp( - alph_low * dose))
 }  
 
@@ -257,3 +257,91 @@ test_runtime <- function(f, ...) {
   f(...)
   Sys.time() - start_time
 }
+
+
+################################################################################
+#' @description Applies Incremental Effect Additivity to a MIXDER.
+#' 
+#' @param dose Numeric vector corresponding to the sum dose in cGy.
+#' @param LET Numeric vector of all LET values, must be length n.
+#' @param ratios Numeric vector of all dose ratios, must be length n.
+#' @param model String value corresponding to the model to be used, either 
+#'              "NTE" or "TE". 
+#' @param coef Named list of numeric vectors containing coefficients for IDERs.
+#' @param ders Named list of functions containing relevant IDER models.
+#' @param calculate_dI Named vector of functions to calculate dI depending on 
+#'                     the selected model.
+#' @param phi Numeric value, used in NTE models.
+#' 
+#' @details Corresponding elements of ratios, LET should be associated with the
+#'          same IDER.
+#'          
+#' @return Numeric vector representing the estimated Harderian Gland 
+#'         prevalence from an IEA MIXDER constructed from the given IDER 
+#'         parameters. 
+#'         
+#' @examples
+#' calculate_id(.01 * 0:40, c(70, 195), c(1/2, 1/2))
+#' calculate_id(.01 * 0:70, c(.4, 195), c(4/7, 3/7), model = "TE")
+
+new_calculate_id <- function(dose, LET, ratios, model = "NTE",
+                         coef = list(NTE = HZE_nte_model_coef, 
+                                     TE = HZE_te_model_coef, 
+                                     lowLET = low_LET_model_coef),
+                         ders = list(NTE = calibrated_HZE_nte_der, 
+                                     TE = calibrated_HZE_te_der, 
+                                     lowLET = calibrated_low_LET_der),
+                         calculate_dI = c(NTE = .calculate_dI_nte, 
+                                          TE = .calculate_dI_te),
+                         phi = 2000) {
+  dE <- function(yini, state, pars) { #  Constructing an ode from the IDERS
+    with(as.list(c(state, pars)), {
+      ############################################
+      # Screen out low LET values
+      lowLET_total <- lowLET_ratio <- 0
+      remove <- c()
+      for (i in 1:length(LET)) {
+        if (LET[i] <= 3) { # ion is low-LET
+          lowLET_total <- lowLET_total + LET[i]
+          lowLET_ratio <- lowLET_ratio + ratios[i]
+          remove <- unique(c(remove, LET[i]))
+          ratios[i] <- 0
+        }
+      }
+      LET <- c(setdiff(LET, remove))
+      ratios <- ratios[! ratios == 0]
+      ############################################
+      
+      aa <- u <- dI <- vector(length = length(LET))
+      if (length(LET) > 0) {
+        for (i in 1:length(LET)) { 
+        aa[i] <- pars[1] * LET[i] * exp( - pars[2] * LET[i])
+        # browser()
+        u[i] <- uniroot(function(dose) HZE_der(dose, LET[i], pars) - I,  
+                        interval = c(0, 20000), 
+                        extendInt = "yes", 
+                        tol = 10 ^ - 10)$root 
+        dI[i] <- ratios[i] * calc_dI(aa[i], u[i], pars[3]) 
+        }
+      }
+      if (lowLET_ratio > 0) { 
+        # If low-LET IDER is present then include it at the end of the dI vector
+        u[length(LET) + 1] <- uniroot(function(dose) low_der(dose, 
+                                      LET = lowLET_total, 
+                                      alph_low = coef[["lowLET"]]) - I, 
+                                      interval = c(0, 20000), 
+                                      extendInt = "yes", 
+                                      tol = 10 ^ - 10)$root
+        dI[length(LET) + 1] <- lowLET_ratio * low_LET_slope(u[length(LET) + 1], 
+                                                            LET = 0)
+      }
+      return(list(sum(dI)))
+    }) 
+  }
+  p <- list(pars = coef[[model]], 
+            HZE_der = ders[[model]], 
+            low_der = ders[["lowLET"]], 
+            calc_dI = calculate_dI[[model]])
+  return(ode(c(I = 0), times = dose, dE, parms = p, method = "radau"))
+}
+
