2417426095b6e064e1b82b19ec3b1654582ec52c
Edward Huang <edwardghg@gmail.com>
2018-06-19 10:10:10 -0600

    Edited attribution information.

--- dataAndInfo.R
+++ dataAndInfo.R
@@ -8,34 +8,36 @@
 # Contact:      Rainer K. Sachs
 # Website:      https://github.com/sachsURAP/NASAmouseHG
 # Mod history:  18 Jun 2018
-# Attribution:  This R script and the scripts that depend on it 
-# (notably synergyTheory.R, monteCarlo.R and plots.R) was developed at UC Berkeley. 
-# 2017-18. An early version was written by Dae Woong Ham Summer 2017. 
-# Additions, corrections, changes, quality control, and major reorganization by Edward G. Huang,
-# Yimin Lin, Mark Ebert, Yunzhi Zhang and Ray Sachs, UCB fall 2017-Summer 2018.
+# Attribution:  This R script and the scripts that depend on it (notably 
+#               synergyTheory.R, monteCarlo.R and plots.R) were developed at 
+#               UC Berkeley 2017-18. An early version was written by 
+#               Dae Woong Ham Summer 2017. Additions, corrections, changes, 
+#               quality control, and major reorganization by Edward G. Huang, 
+#               Yimin Lin, Mark Ebert, Yunzhi Zhang and Ray Sachs, 
+#               UCB Fall 2017 - Summer 2018.
 
 # Relevant references and abbreviations:
-#
+
 #   ".93Alp" = Alpen et al. "Tumorigenic potential of high-Z, high-LET charged-
 #                           particle radiations." Rad Res 136:382-391 (1993).
-#
+
 #   ".94Alp" = Alpen et al. "Fluence-based relative biological effectiveness for
 #                           charged particle carcinogenesis in mouse Harderian 
 #                           gland." Adv Space Res 14(10): 573-581. (1994).  
-#
+
 #   "16Chang" = Chang et al. "Harderian Gland Tumorigenesis: Low-Dose and LET 
 #                            Response." Radiat Res 185(5): 449-460. (2016). 
-#
+
 #   "16Srn" = Siranart et al. "Mixed Beam Murine Harderian Gland Tumorigenesis: 
 #                             Predicted Dose-Effect Relationships if neither 
 #                             Synergism nor Antagonism Occurs." 
 #                             Radiat Res 186(6): 577-591 (2016).  
-#
+
 #   "17Cuc" = Cucinotta & Cacao. "Non-Targeted Effects Models Predict 
 #                                Significantly Higher Mars Mission Cancer Risk 
 #                                than Targeted Effects Models." 
 #                                Sci Rep 7(1): 1832. (2017). PMC5431989
-#
+
 #   "HZE" = High atomic number and energy
 #   "LET" = Linear energy transfer
 #   "NTE" = Non-targeted effects
